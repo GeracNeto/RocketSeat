27feb931da97fc0feb4bf0074da984fab41e4601
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-07-28 12:25:46 -0300

    Feito exercício sobre variáveis

---
+++ Javascript/exercicio_variaveis.js
@@ -0,0 +1,19 @@
+let weight;
+
+// Depende do que eu atribuir à ela, no momento ela é do tipo undefined
+
+weight = 72
+
+//Agora a variável é do tipo number integer
+
+console.log(typeof(weight));
+
+const person ={
+    name: 'Geraldo',
+    age: 23,
+    stars: 5.0,
+    isSubscribed: true
+}
+
+
+console.log(person.name);
