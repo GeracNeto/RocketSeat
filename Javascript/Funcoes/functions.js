a73403b0cb466d31a5b71fd1e454127efac8aa4c
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-07-29 19:19:57 -0300

    Criado função anônima que retorna a soma de dois números

---
+++ Javascript/Funcoes/functions.js
@@ -0,0 +1,21 @@
+// Criar um aplicativo de frases motivacionais
+
+function createPhrases(){
+    console.log('Estudar é muito bom');
+    console.log('Paciência e persistência');
+    console.log('Revisão é mãe do aprendizado');
+}
+
+// Código reutilizado
+createPhrases();
+
+// Declaração de função em variável
+
+// Função anônima
+
+const sum = function(number1, number2){
+    let total = number1 + number2;
+    return console.log(`Soma: ${total}`);
+}
+
+sum(2,3);
