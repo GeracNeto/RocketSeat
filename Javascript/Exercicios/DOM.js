b56ffe969e1cf562a52faded56c6f1b6df2e702f
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-22 15:37:23 -0300

    Criado exemplo do uso do querySelector

--- Javascript/Exercicios/DOM.js
+++ Javascript/Exercicios/DOM.js
@@ -9,3 +9,7 @@ console.log(document.getElementsByClassName('one'))
 // getElementByTagName()
 
 console.log(document.getElementsByTagName('p'))
+
+// querySelector() - Pega o primeiro seletor que ele enontrar na página
+
+console.log(document.querySelector('[type]'))
