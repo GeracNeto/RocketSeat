8c706f9f4104b5f6abe8f4d225824ceba52a765c
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-22 15:33:00 -0300

    Criado exemplo do uso do getElementByTagName

--- Javascript/Exercicios/DOM.js
+++ Javascript/Exercicios/DOM.js
@@ -5,3 +5,7 @@ console.log(document.getElementById('blog-title'))
 // getElementByClassName()
 
 console.log(document.getElementsByClassName('one'))
+
+// getElementByTagName()
+
+console.log(document.getElementsByTagName('p'))
