a32e6f40b102ffc16c6c1b3b3de6759f2567df5c
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-05 19:31:10 -0300

    Criado exemplos de manipulação de conteúdos na DOM

---
+++ Javascript/DOM/Manipulando_Conteudos/script.js
@@ -0,0 +1,20 @@
+// textContent
+const element = document.getElementById("textContent")
+element.textContent = 'Ola devs'
+
+// innertext
+const element1 = document.getElementById("innerText")
+element1.innerText = 'Ola devs'
+
+// innerHTML -> permite utilzar HTML no elemento
+const element2 = document.getElementById("innerHTML")
+element2.innerHTML = "Olá devs <small>!!!</small>"
+
+// value
+const element3 = document.getElementById("value")
+element3.value = 5
+
+// Atributos
+const header = document.querySelector("header")
+header.setAttribute('id', 'homeHeader') // Atribui ID para o elemento header
+
